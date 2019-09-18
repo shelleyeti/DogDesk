f7e706c25de04e242e313df5a95a66c2ce6ff356
VPN Shelley <shell@CoreyA.(none)>
2019-09-18 10:49:29 -0500

    set up for landing

--- DogDesk/Controllers/HomeController.cs
+++ DogDesk/Controllers/HomeController.cs
@@ -5,14 +5,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DogDesk.Models;
+using DogDesk.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace DogDesk.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IHostingEnvironment _appEnvironment;
+
+        public HomeController(ApplicationDbContext context,
+                          UserManager<ApplicationUser> userManager,
+                          IHostingEnvironment appEnvironment)
         {
-            return View();
+            _userManager = userManager;
+            _context = context;
+            _appEnvironment = appEnvironment;
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+        public async Task<IActionResult> Index()
+        {
+            var pet = await _context.Pets
+                .Include(p => p.GenderOfAnimal)
+                .Include(p => p.SizeOfAnimal)
+                .Include(p => p.TypeOfAnimal)
+                .Include(p => p.PetOwners)
+                .Include(p => p.VetRecords)
+                .Include(p => p.PetContacts)
+                .Include(p => p.ServicePets)
+                .ThenInclude(x => x.NameOfService)
+                .FirstOrDefaultAsync();
+
+            pet.PetOwners.ToList().ForEach(x => x.Owner = _context.Owners.FirstOrDefault(y => y.Id == x.OwnerId));
+            pet.PetContacts.ToList().ForEach(x => x.EmergencyContact = _context.EmergencyContacts.FirstOrDefault(y => y.Id == x.EmergencyContactId));
+            pet.VetRecords = pet.VetRecords.OrderByDescending(x => x.Id).Take(3).ToList();
+
+            if (pet == null)
+            {
+                return NotFound();
+            }
+
+            return View(pet);
         }
 
         public IActionResult Privacy()
